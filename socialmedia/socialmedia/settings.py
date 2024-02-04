c52cff0bb72d5c0ea155a78fb4963e10e4a15674
Ankur Singh <157984590+ankurjava12@users.noreply.github.com>
2024-02-04 18:21:21 +0530

    Update settings.py

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
